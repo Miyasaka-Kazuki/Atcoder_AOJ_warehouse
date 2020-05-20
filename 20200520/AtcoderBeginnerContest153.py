0bc9e70e5cccbc35829e92227371e6bde18432c4
Kazuki Miyasaka <kazuki@KazukinoMacBook-Pro.local>
2020-05-20 14:10:22 +0900

    20200520-2

--- 20200520/AtcoderBeginnerContest153.py
+++ 20200520/AtcoderBeginnerContest153.py
@@ -23,3 +23,51 @@ for i in range(N): #ナップザック問題の動的計画法
         nj = max(j - A, 0) # 配列外参照防止
         dp[j] = min(dp[j], dp[nj] + B)
 print(dp[H])
+
+
+#C++に変えたら全部できたので上のコードもおそらく正しい
+
+#pythonの上のコード
+# #include <iostream>
+# #include <bits/stdc++.h>
+# using namespace std;
+# const int INF = 1001001001;
+
+# int main(void){
+#     int H, N;
+#     cin >> H >> N;
+#     vector<int> dp(H + 1, INF);
+#     dp[0] = 0;
+#     for(int i = 0; i < N; i++){
+#         int A, B;
+#         cin >> A >> B;
+#         for(int j = 0; j < H; j++){
+#             int nj = min(j + A, H);
+#             dp[nj] = min(dp[nj], dp[j] + B);
+#         }
+#     }
+#     cout << dp[H] << endl;
+# }
+
+
+#pythonの下のコード
+# #include <iostream>
+# #include <bits/stdc++.h>
+# using namespace std;
+# const int INF = 1001001001;
+
+# int main(void){
+#     int H, N;
+#     cin >> H >> N;
+#     vector<int> dp(H + 1, INF);
+#     dp[0] = 0;
+#     for(int i = 0; i < N; i++){
+#         int A, B;
+#         cin >> A >> B;
+#         for(int j = 1; j <= H; j++){
+#             int nj = max(j - A, 0);
+#             dp[j] = min(dp[j], dp[nj] + B);
+#         }
+#     }
+#     cout << dp[H] << endl;
+# }
